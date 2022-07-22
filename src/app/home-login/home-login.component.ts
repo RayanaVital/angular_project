153c1b71812fd45e3aafa5579e19c7ca5cf304ca
Rayana Vital <rayanavital@gmail.com>
2022-07-22 09:56:16 -0300

    Add initial arquives

---
+++ src/app/home-login/home-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-login',
+  templateUrl: './home-login.component.html',
+  styleUrls: ['./home-login.component.scss']
+})
+export class HomeLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
