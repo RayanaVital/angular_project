19dcbecd3779de6517036241e06b3b6e0622e3ea
Rayana Vital <rayanavital@gmail.com>
2022-07-24 12:04:54 -0300

    Add homw route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', loadChildren:()=>import('./login/login.module').then((m)=>m.LoginModule) },
+  {path: 'home', loadChildren:()=>import('./home/home.module').then((m)=>m.HomeModule)},
 ];
 
 @NgModule({
--- src/app/login/login-acess/login-acess.component.ts
+++ src/app/login/login-acess/login-acess.component.ts
@@ -1,5 +1,6 @@
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-acess',
@@ -11,14 +12,14 @@ export class LoginAcessComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
-    console.log('Login realizado com sucesso')
+      this.router.navigate(['/home']);
     },(error) => {
       alert('Erro ao realizar login');
       console.log(error);
