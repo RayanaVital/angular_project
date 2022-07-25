bb6d0fbbe13bd6e677b794ad1e1627f364994dc8
Rayana Vital <rayanavital@gmail.com>
2022-07-25 11:21:49 -0300

    Add user services validations

---
+++ src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,45 @@
+import { TokenService } from './../token.service';
+import { Injectable } from '@angular/core';
+import { jwt_decode } from "jwt-decode";
+import { Usuario } from './usuario';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private usuarioSubject = new BehaviorSubject<Usuario>({});
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.possuiToken()){
+      this.decodificaJWT();
+    }
+   }
+
+  private decodificaJWT(){
+    const token = this.tokenService.retornaToken();
+    const usuario = jwt_decode(token) as Usuario;
+    this.usuarioSubject.next(usuario);
+  }
+
+  retornaUsuario(){
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string){
+    this.tokenService.salvaToken(token);
+    this.decodificaJWT();
+  }
+
+  logout(){
+    this.tokenService.excluiToken();
+    this.usuarioSubject.next({});
+  }
+
+  estaLogado(){
+    return this.tokenService.possuiToken();
+  }
+}
+
+
