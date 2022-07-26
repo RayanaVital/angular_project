c1b58847b0b80a5dff1ae280d7db8266731635fd
Rayana Vital <rayanavital@gmail.com>
2022-07-26 11:23:53 -0300

    Add dashboard page and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', loadChildren:()=>import('./login/login.module').then((m)=>m.LoginModule) },
   {path: 'home', loadChildren:()=>import('./home/home.module').then((m)=>m.HomeModule)},
+  {path: 'dashboard', loadChildren:()=>import('./dashboard/dashboard.module').then((m)=>m.DashboardModule)},
 ];
 
 @NgModule({
