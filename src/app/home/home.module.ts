b0bed614ebcbff299960d8dea5df822f601ec903
Rayana Vital <rayanavital@gmail.com>
2022-07-31 17:48:20 -0300

    Add img background and card component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
